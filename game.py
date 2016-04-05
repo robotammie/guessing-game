a288d406f9cf98153fb5f0a4850472707d1a3f68
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 12:59:26 -0700

    Counting Tries

--- game.py
+++ game.py
@@ -8,12 +8,15 @@ print "I'm thinking of a number between 1 and 100."
 
 secret_number = random.randint(1, 100)
 print secret_number
+numbers_guessed = []
 
 while True:
     guess = int(raw_input("Enter your guess: "))
+    numbers_guessed = numbers_guessed + [guess]
+    # print numbers_guessed
 
     if guess == secret_number:
-        print "Congratulations, you guessed it!"
+        print "Congratulations, you guessed it the number in %d tries!" % (len(numbers_guessed))
         break
 
     elif guess > secret_number:
