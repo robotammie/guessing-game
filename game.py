3f6a270cd05367786dd1766727be8ebfb33bde20
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 15:42:19 -0700

    moved value error to aid in readability

--- game.py
+++ game.py
@@ -13,32 +13,31 @@ def play_game():
     while True:
         try:
             guess = int(raw_input("Enter your guess: "))
-            # print numbers_guessed
-
-            if guess < 1 or guess > 100:
-                print "That is not a number between 1 and 100, try again."
-
-            else:
-                if guess in numbers_guessed:
-                    print "You've guessed this number before."
-                else:
-                    numbers_guessed = numbers_guessed + [guess]
-
-
-                if guess == secret_number:
-                    score = len(numbers_guessed)
-                    if best_score == None or score < best_score:
-                        best_score = score
-                    print "Congratulations, you guessed my number in %d tries!" % (score)
-                    break
-                elif guess > secret_number:
-                    print "Your guess is too high, try again."
-                else:
-                    print "Your guess is too low, try again."
-
         except ValueError:
             print "Please try again with an integer between 1 and 100."
 
+        if guess < 1 or guess > 100:
+            print "That is not a number between 1 and 100, try again."
+
+        else:
+            if guess in numbers_guessed:
+                print "You've guessed this number before."
+            else:
+                numbers_guessed = numbers_guessed + [guess]
+
+
+            if guess == secret_number:
+                score = len(numbers_guessed)
+                if best_score == None or score < best_score:
+                    best_score = score
+                print "Congratulations, you guessed my number in %d tries!" % (score)
+                break
+            elif guess > secret_number:
+                print "Your guess is too high, try again."
+            else:
+                print "Your guess is too low, try again."
+
+        
     play_again = raw_input("Would you like to play again? y/n ")
     if play_again == 'y' or play_again == 'yes':
         play_game()
