29aeba1f52311464a026577e4fd91a66d9035184
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 14:49:27 -0700

    Handling non-integers

--- game.py
+++ game.py
@@ -11,23 +11,26 @@ print secret_number
 numbers_guessed = []
 
 while True:
-    guess = int(raw_input("Enter your guess: "))
-    print numbers_guessed
+    try:
+        guess = int(raw_input("Enter your guess: "))
+        print numbers_guessed
 
-    if guess in numbers_guessed:
-        print "You've guessed this number before, try again."
+        if guess in numbers_guessed:
+            print "You've guessed this number before, try again."
 
-    elif guess < 1 or guess > 100:
-        print "That is not a number between 1 and 100, try again."
-
-    else:
-        numbers_guessed = numbers_guessed + [guess]
-        if guess == secret_number:
-            print "Congratulations, you guessed my number in %d tries!" % (len(numbers_guessed))
-            break
-
-        elif guess > secret_number:
-            print "Your guess is too high, try again."
+        elif guess < 1 or guess > 100:
+            print "That is not a number between 1 and 100, try again."
 
         else:
-            print "Your guess is too low, try again."
+            numbers_guessed = numbers_guessed + [guess]
+            if guess == secret_number:
+                print "Congratulations, you guessed my number in %d tries!" % (len(numbers_guessed))
+                break
+
+            elif guess > secret_number:
+                print "Your guess is too high, try again."
+
+            else:
+                print "Your guess is too low, try again."
+    except ValueError:
+        print "Please try again with an integer between 1 and 100."
