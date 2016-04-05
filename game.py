8dde56b779f13c2ab31292196d023987e6e5d2aa
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 12:47:12 -0700

    Secret Number Generated

--- game.py
+++ game.py
@@ -3,3 +3,8 @@ import random
 
 print "Hello, welcome to the Guessing Game!"
 name = raw_input("What is your name? ")
+print "Hello, %s!" % (name)
+print "I'm thinking of a number between 1 and 100."
+
+secret_number = random.randint(1, 100)
+print secret_number
