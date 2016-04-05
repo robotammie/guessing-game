543bab05d18e01f08f86601245a405b959b0bedc
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 15:04:37 -0700

    Feedback on already guessed number

--- game.py
+++ game.py
@@ -7,22 +7,24 @@ print "Hello, %s!" % (name)
 print "I'm thinking of a number between 1 and 100."
 
 secret_number = random.randint(1, 100)
-print secret_number
+# print secret_number
 numbers_guessed = []
 
 while True:
     try:
         guess = int(raw_input("Enter your guess: "))
-        print numbers_guessed
+        # print numbers_guessed
 
-        if guess in numbers_guessed:
-            print "You've guessed this number before, try again."
-
-        elif guess < 1 or guess > 100:
+        if guess < 1 or guess > 100:
             print "That is not a number between 1 and 100, try again."
 
         else:
-            numbers_guessed = numbers_guessed + [guess]
+            if guess in numbers_guessed:
+                print "You've guessed this number before."
+
+            else:
+                numbers_guessed = numbers_guessed + [guess]
+
             if guess == secret_number:
                 print "Congratulations, you guessed my number in %d tries!" % (len(numbers_guessed))
                 break
@@ -32,5 +34,6 @@ while True:
 
             else:
                 print "Your guess is too low, try again."
+                
     except ValueError:
         print "Please try again with an integer between 1 and 100."
