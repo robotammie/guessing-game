00654c61a2b3387ad0c406af7d3cce38b4ac59ef
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 12:53:30 -0700

    Checking the Guess

--- game.py
+++ game.py
@@ -8,3 +8,16 @@ print "I'm thinking of a number between 1 and 100."
 
 secret_number = random.randint(1, 100)
 print secret_number
+
+while True:
+    guess = int(raw_input("Enter your guess: "))
+
+    if guess == secret_number:
+        print "Congratulations, you guessed it!"
+        break
+
+    elif guess > secret_number:
+        print "Your guess is too high, try again."
+
+    else:
+        print "Your guess is too low, try again."
